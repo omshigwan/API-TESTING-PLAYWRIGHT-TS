48b366eacaaac6a74fa5b64ab0da7f6a585cf4cc
Om Shigwan <omds2002@gmail.com>
2026-02-14 04:03:03 +0530

    fix: action reporting and test run

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   testDir: "tests",
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 2,
+  retries: process.env.CI ? 1 : 2,
   workers: 1,
   reporter: [
     ["list"],
